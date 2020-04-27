fc8ac54c4b7ee3c8c88ef20452fac264dd445885
abishek <abishek.g@mrcooper.com>
2020-04-27 23:30:07 +0530

    Link to different pages

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const AboutPage = () => {
   return(<div>
     <h1>About</h1>
     <p>Bio for About Page</p>
+    <p>Need a developer? <Link to='/contact'>Contact Me</Link> </p>
   </div>)
 }
 
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 
 const ContactPage = () => {
-  return(<div>
+  return (<div>
     <h1>Contact</h1>
     <p>Details for Contact Page</p>
+    <p>Link to my <a href='https://github.com/abishekg/gatsby-bootcamp' target='_blank'>Github</a> profile</p>
   </div>)
 }
 
-export default ContactPage;
+export default ContactPage

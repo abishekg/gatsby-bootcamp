239694032331b5a2c48112e1c1d7262b9d704a40
abishek <abishek.g@mrcooper.com>
2020-04-28 00:19:45 +0530

    Using CSS Modules

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react"
 import Header from "../header"
 import Footer from "../footer"
-import '../../styles/index.scss';
+import "../../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Header />
-      {children}
-      <Footer />
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header/>
+        {children}
+      </div>
+      <Footer/>
     </div>
   )
 }
 
-export default Layout;
+export default Layout
